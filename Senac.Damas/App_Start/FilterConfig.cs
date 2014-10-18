5b4ad55f9de65f70e58cb8a43f9e75705742aa48
jacksonfraga <jackson@jacksonfraga.com.br>
2014-10-18 14:27:27 -0300

    Criação do projeto

---
+++ Senac.Damas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Senac.Damas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Senac.Damas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Senac.Damas.MvcApplication" Language="C#" %>
